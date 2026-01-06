9911d76f9f2b625ab190227f0c17de9758bd164a
Arijit Sarkar <sarkararijit1as@gmail.com>
2026-01-06 21:54:33 +0530

    third

---
+++ YT9.java
@@ -0,0 +1,13 @@
+public class YT9 {
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int number = 5;
+        System.out.println("Factorial of " + number + " is: " + factorial(number));
+    }
+}
